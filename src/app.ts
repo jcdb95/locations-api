d4e40eebce918d2ae8259eda39e40f2d9bb31b5f
jcdb95 <juanchitkd95@gmail.com>
2021-10-04 11:41:50 +0200

    +trying to solve cors issue

--- src/app.ts
+++ src/app.ts
@@ -30,7 +30,7 @@ app.use((
 /** Rules of our API */
 app.use((req: Request, res: Response, next: NextFunction) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization, Access-Control-Allow-Headers, X-Requested-With');
 
     if (req.method == 'OPTIONS') {
         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
