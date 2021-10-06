1713e53af2b00bbe450a3c586e21a5cb9148e4bc
jcdb95 <juanchitkd95@gmail.com>
2021-10-06 09:39:52 +0200

    +testing cors

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,7 @@ const app = express();
 const port = process.env.PORT || 3000
 
 app.use(json());  
+// app.use(cors());
 
 app.use('/api/locations', LocationRoutes);
 
@@ -22,26 +23,25 @@ app.get('/api/auth', (req: Request, res: Response) =>{
     }
 });
 
-app.use(cors())
 
-// /** Rules of our API */
-// app.use((req: Request, res: Response, next: NextFunction) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization");
-//     res.header('Content-Type: application/json');
-//     res.header('Accept: application/json');
+/** Rules of our API */
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Headers", 'X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization');
+    res.header('Content-Type: application/json');
+    // res.header('Accept: application/json');
     
     
-//     // OPTIONS is the Preflight method dispatch 
-//     if (req.method == 'OPTIONS') {
-//         res.header("Access-Control-Allow-Methods: HEAD, GET, POST, PUT, PATCH, DELETE, OPTIONS");
-//         res.header('Content-Type: application/json');
-//         res.header('Access-Control-Allow-Headers: Content-Type'); 
-//         return res.status(200)
-//     }
+    // OPTIONS is the Preflight method dispatch 
+    if (req.method === 'OPTIONS') {
+        res.header("Access-Control-Allow-Methods: HEAD, GET, POST, PUT, PATCH, DELETE, OPTIONS");
+        res.header('Content-Type: application/json');
+        res.header('Access-Control-Allow-Headers: Content-Type'); 
+        return res.status(200)
+    }
 
-//     next();
-// });
+    next();
+});
 
 
 /** Error handling */
